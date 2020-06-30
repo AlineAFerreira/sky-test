419cdd9adde5d3f388da774ed39b52d3491a243c
Aline Ferreira <aline.ferreira@bpf.com.br>
2020-06-30 01:21:54 -0300

    Gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const {src, dest, watch, series, parallel} = require('gulp');
 const replace = require('gulp-replace');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify-es').default;
+const clean = require('gulp-clean');
 
 
 const files = {
@@ -14,19 +15,19 @@ const files = {
 function sassTask(){
   return src(files.sassPath)
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(dest('dist/assets'));
+    .pipe(dest('dist/css'));
 }
 
 function jsTask(){
   return src(files.jsPath)
     .pipe(uglify())
-    .pipe(dest('dist'));
+    .pipe(dest('dist/js'));
 }
 
 const cbString = new Date().getTime();
 
 function cacheBustTask(){
-  return src('src/index.html')
+  return src('index.html')
     .pipe(replace(/v=\d+/g, 'v=' + cbString))
     .pipe(dest('.'))
 }
@@ -34,10 +35,21 @@ function cacheBustTask(){
 function watchTask(){
   watch([files.sassPath, files.jsPath], 
     parallel(sassTask, jsTask));
-
 }
 
+function cleanDist(){
+  return src('dist')
+  .pipe(clean());
+}
+
+exports.build = series(
+  cleanDist,
+  parallel(sassTask, jsTask),
+  cacheBustTask,
+)
+
 exports.default = series(
+  cleanDist,
   parallel(sassTask, jsTask),
   cacheBustTask,
   watchTask
